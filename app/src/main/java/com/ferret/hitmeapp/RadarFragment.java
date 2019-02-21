b3bc847cfd04dc66fc09679aa36a3ac8c4dd7101
Ferran Ramírez <ferranramireznavajon@gmail.com>
2019-02-21 01:59:03 +0100

    RadarFragment gets and updates location. This closes #10

--- app/src/main/java/com/ferret/hitmeapp/RadarFragment.java
+++ app/src/main/java/com/ferret/hitmeapp/RadarFragment.java
@@ -1,26 +1,36 @@
 package com.ferret.hitmeapp;
 
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class RadarFragment extends Fragment implements OnMapReadyCallback {
+public class RadarFragment extends Fragment implements OnMapReadyCallback, LocationListener {
 
-    private MapView mapView;
     private GoogleMap mMap;
+
+    private LocationManager locationManager;
+    private long minTime = 500;
+    private float minDistance = 1;
+    private static final int MY_PERMISSION_GPS = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,21 +41,86 @@ public class RadarFragment extends Fragment implements OnMapReadyCallback {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View root = inflater.inflate(R.layout.activity_maps, container, false);
 
+
+        setUpLocation();
         SupportMapFragment mMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mMapFragment!= null) {
+        if (mMapFragment != null) {
             mMapFragment.getMapAsync(this);
         }
+
         return root;
     }
 
+    private void setUpLocation() {
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(RadarFragment.this.getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(RadarFragment.this.getActivity(),
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_GPS);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, minTime, minDistance, this);
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_GPS:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // All good!
+                } else {
+                    Toast.makeText(this.getContext(), "Need your location!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng barcelonaLocation = new LatLng(41.3851, 2.1734);
+        Log.i("onMapReady", "Location set to Barcelona");
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(barcelonaLocation, 12.0f));
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    @Override
+    public void onLocationChanged(Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        Log.i("onLocationChanger", "Location set to " + latitude + ", " + " longitude");
+        LatLng currentLocation = new LatLng(latitude, longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        setUpLocation();
+        Log.i("onResume", "Restarted location updates");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        locationManager.removeUpdates(this);
+        Log.i("onPause", "Stopped location updates");
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
